29ab17166482b689ee39db0fbe11c323cd57b9fd
pat-bee <patrick.burritt@gmail.com>
2024-01-15 14:49:24 -0500

    Update main.js
    removed script tags

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
- <script>
+
     var today = new Date();
     var formattedDate = today.toLocaleDateString('en-US', {
     day: 'long',
     month: 'long',
     year: 'numeric'
 });
-    </script>
+ 
