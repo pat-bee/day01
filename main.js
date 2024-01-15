5299168dd562bdf18c9828a4c70805bea1f8714e
pat-bee <patrick.burritt@gmail.com>
2024-01-15 16:21:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+   document.getElementById('myForm').addEventListener('submit', function(e) {
+       e.preventDefault();
+
+       var field1 = document.getElementById('field1').value;
+       var category = document.getElementById('category').value;
+
+       google.script.run.myFunction(field1, category);
+   });
+
 
     var today = new Date();
     var formattedDate = today.toLocaleDateString('en-US', {
